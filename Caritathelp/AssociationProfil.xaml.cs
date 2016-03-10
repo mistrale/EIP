7588888a843d797ced72d2245c0eaabaeb3f5142
mistrale <dsikorav6@gmail.com>
2016-03-10 20:47:50 +0100

    profil association

--- Caritathelp/AssociationProfil.xaml.cs
+++ Caritathelp/AssociationProfil.xaml.cs
@@ -31,6 +31,15 @@ namespace Caritathelp
     {
         private string id;
 
+        class Member
+        {
+            public string id { get; set; } 
+            public string mail { get; set; }
+            public string firstname { get; set; }
+            public string lastname { get; set; }
+            public string rights { get; set; }
+        }
+
         class   AssociationRequest
         {
             public string status { get; set; }
@@ -38,7 +47,91 @@ namespace Caritathelp
             public Association response { get; set; }
         }
 
+        class MemberRequest
+        {
+            public string status { get; set; }
+            public string message { get; set; }
+            public IList<Member> response { get; set; }
+        }
+
+        private Grid membersGrid;
+        private Grid eventsGrid;
         private AssociationRequest assoc;
+        private MemberRequest members;
+
+
+        private async void getMember()
+        {
+            var httpClient = new HttpClient(new HttpClientHandler());
+            try
+            {
+                var template = new UriTemplate("http://52.31.151.160:3000/associations/" + id + "/members" + "{?token}");
+                template.AddParameter("token", (string)Windows.Storage.ApplicationData.Current.LocalSettings.Values["token"]);
+                var uri = template.Resolve();
+                Debug.WriteLine(uri);
+
+                HttpResponseMessage response = await httpClient.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+                responseString = await response.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine(responseString.ToString());
+                members = JsonConvert.DeserializeObject<MemberRequest>(responseString);
+                if (Int32.Parse(members.status) != 200)
+                {
+
+                }
+                else
+                {
+                    Debug.WriteLine(members.response.Count);
+                    membersGrid.Height = members.response.Count * 100;
+                    membersGrid.Width = 375;
+                    for (int i = 0; i < members.response.Count; ++i)
+                        membersGrid.RowDefinitions.Add(new RowDefinition());
+                    membersGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                    for (int x = 0; x < members.response.Count; ++x)
+                    {
+                        Button btn = new Button();
+                        btn.Height = 100;
+                        btn.Width = membersGrid.Width;
+                        btn.HorizontalAlignment = HorizontalAlignment.Stretch;
+                        btn.Click += new RoutedEventHandler(UserButtonClick);
+                        btn.Content = members.response[x].firstname + " " + members.response[x].lastname;
+                        btn.Background = new SolidColorBrush(Color.FromArgb(0xFF, 124, 188, 99));
+                        Grid.SetColumn(btn, 1);
+                        Grid.SetRow(btn, x);
+                        membersGrid.Children.Add(btn);
+                        membersGrid.Visibility = Visibility.Visible;
+                    }
+                }
+            }
+            catch (HttpRequestException e)
+            {
+            }
+            catch (JsonReaderException e)
+            {
+                System.Diagnostics.Debug.WriteLine(responseString);
+                Debug.WriteLine("Failed to read json");
+            }
+            catch (JsonSerializationException e)
+            {
+                Debug.WriteLine(responseString);
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+        }
+
+        private void UserButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            int x = Grid.GetRow(button);
+            string id = members.response[x].id.ToString();
+            Frame.Navigate(typeof(Profil), id);
+            // identify which button was clicked and perform necessary actions
+        }
+
+        public void memberClick(object sender, RoutedEventArgs e)
+        {
+            scroll.Content = membersGrid;
+            membersGrid.Visibility = Visibility.Visible;
+        }
 
         private async void getInformation()
         {
@@ -62,7 +155,6 @@ namespace Caritathelp
                 else
                 {
                     titleText.Text = assoc.response.name;
-                    descriptionText.Text = assoc.response.description;
                 }
             }
             catch (HttpRequestException e)
@@ -83,6 +175,8 @@ namespace Caritathelp
         public AssociationProfil()
         {
             this.InitializeComponent();
+            membersGrid = new Grid();
+            eventsGrid = new Grid();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -92,6 +186,7 @@ namespace Caritathelp
             Debug.WriteLine(id);
      //       notifs = JsonConvert.DeserializeObject<Notifications>((string)Windows.Storage.ApplicationData.Current.LocalSettings.Values["notifications"]);
             getInformation();
+            getMember();
         }
 
         private void searchTextBox_GotFocus(object sender, RoutedEventArgs e)
